e06cddea0e8114809d6f22245de1a3411d4ada55
Alfred GOUMOU <45146834+fredgoum@users.noreply.github.com>
2025-04-07 16:38:16 +0200

    New translations lang.php (English, United States)

---
+++ en-US/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'welcome' => 'Bienvenue sur notre plateforme !',
+    'goodbye' => 'Au revoir et à bientôt.',
+    'error_404' => 'Page non trouvée.',
+    'reset_password' => 'Réinitialiser le mot de passe',
+    'validation_failed' => 'La validation a échoué. Veuillez vérifier vos informations.',
+];
