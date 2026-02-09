1e5e65de4752a8c985780362fef12c47f2a1ead8
Alfred GOUMOU <45146834+fredgoum@users.noreply.github.com>
2026-02-09 11:47:49 +0100

    New translations lang.php (German)

--- de/lang.php
+++ de/lang.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'welcome' => 'Bienvenue sur notre plateforme !',
+    'welcome' => '!',
     'goodbye' => 'Au revoir et à bientôt.',
     'error_404' => 'Page non trouvée.',
     'reset_password' => 'Réinitialiser le mot de passe',
